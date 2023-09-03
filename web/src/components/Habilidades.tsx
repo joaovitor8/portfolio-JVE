098d6353f7150a877e58de58e5094a909d9d92e0
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-09-03 17:23:34 -0300

    icons

--- web/src/components/Habilidades.tsx
+++ web/src/components/Habilidades.tsx
@@ -8,28 +8,28 @@ export const Habilidades = () => {
         <div className="flex flex-col items-center">
           <h3 className="text-3xl">Linguagens</h3>
           <div className="flex">
-            <Image src={"/icons/linguagens/html5.png"} height={70} width={70} alt={""} className="hover:bg-blue-500 rounded-md p-3"/>
-            <Image src={"/icons/linguagens/css3.png"} height={70} width={70} alt={""} className="hover:bg-orange-500 rounded-md p-3"/>
-            <Image src={"/icons/linguagens/javascript.png"} height={70} width={70} alt={""} className="hover:bg-yellow-500 rounded-md p-3"/>
-            <Image src={"/icons/linguagens/typescript.png"} height={70} width={70} alt={""} className="hover:bg-blue-600 rounded-md p-3"/>
-            <Image src={"/icons/linguagens/python.png"} height={70} width={70} alt={""} className="hover:bg-gradient-to-br from-blue-600 to-yellow-600 rounded-md p-3"/>
+            <Image src={"/html5.png"} height={70} width={70} alt={""} className="hover:bg-blue-500 rounded-md p-3"/>
+            <Image src={"/css3.png"} height={70} width={70} alt={""} className="hover:bg-orange-500 rounded-md p-3"/>
+            <Image src={"/javascript.png"} height={70} width={70} alt={""} className="hover:bg-yellow-500 rounded-md p-3"/>
+            <Image src={"/typescript.png"} height={70} width={70} alt={""} className="hover:bg-blue-600 rounded-md p-3"/>
+            <Image src={"/python.png"} height={70} width={70} alt={""} className="hover:bg-gradient-to-br from-blue-600 to-yellow-600 rounded-md p-3"/>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <h3 className="text-3xl">Tecnologias</h3>
           <div className="flex">
-            <Image src={"/icons/tecnologias/react.png"} height={70} width={70} alt={""} className="hover:bg-blue-400 rounded-md p-3"/>
-            <Image src={"/icons/tecnologias/nextjs.png"} height={70} width={70} alt={""} className="hover:bg-gray-600 rounded-md p-3"/>
-            <Image src={"/icons/tecnologias/tailwindcss.png"} height={70} width={70} alt={""} className="hover:bg-blue-600 rounded-md p-3"/>
-            <Image src={"/icons/tecnologias/nodejs.png"} height={70} width={70} alt={""} className="hover:bg-green-700 rounded-md p-3"/>
+            <Image src={"/react.png"} height={70} width={70} alt={""} className="hover:bg-blue-400 rounded-md p-3"/>
+            <Image src={"/nextjs.png"} height={70} width={70} alt={""} className="hover:bg-gray-600 rounded-md p-3"/>
+            <Image src={"/tailwindcss.png"} height={70} width={70} alt={""} className="hover:bg-blue-600 rounded-md p-3"/>
+            <Image src={"/nodejs.png"} height={70} width={70} alt={""} className="hover:bg-green-700 rounded-md p-3"/>
           </div>
         </div>
         <div className="flex flex-col items-center">
           <h3 className="text-3xl">Ferramentas</h3>
           <div className="flex">
-            <Image src={"/icons/ferramentas/git.png"} height={70} width={70} alt={""} className="hover:bg-red-600 rounded-md p-3"/>
-            <Image src={"/icons/ferramentas/visual-studio.png"} height={70} width={70} alt={""} className="hover:bg-blue-500 rounded-md p-3"/>
-            <Image src={"/icons/ferramentas/mongodb.png"} height={70} width={70} alt={""} className="hover:bg-green-500 rounded-md p-3"/>
+            <Image src={"/git.png"} height={70} width={70} alt={""} className="hover:bg-red-600 rounded-md p-3"/>
+            <Image src={"/visual-studio.png"} height={70} width={70} alt={""} className="hover:bg-blue-500 rounded-md p-3"/>
+            <Image src={"/mongodb.png"} height={70} width={70} alt={""} className="hover:bg-green-500 rounded-md p-3"/>
           </div>
         </div>
       </div>
