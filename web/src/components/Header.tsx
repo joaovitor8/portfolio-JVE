0e1da98c0a72cfeaa242109cb2489c331cb89e21
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-09-04 21:46:15 -0300

    Update Header.tsx

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export const Header = () => {
   return (
-    <header className="h-16 w-full flex items-center justify-between px-10 fixed">
+    <header className="h-16 w-full flex items-center justify-between px-10 fixed bg-black">
       <h1 className="text-4xl">&lt;DEV/&gt;</h1>
       <nav className="space-x-10">
         <Link href={""} className="hover:underline decoration-green-500">Home</Link>
