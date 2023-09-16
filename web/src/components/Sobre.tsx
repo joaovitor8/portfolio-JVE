044f3b3c0cc28becc6d0a0e51bc66148ac6da6ed
João Vitor Ezequiel <joaoezeki@gmail.com>
2023-09-16 11:54:33 -0300

    atualizacao

--- web/src/components/Sobre.tsx
+++ web/src/components/Sobre.tsx
@@ -6,9 +6,9 @@ export const Sobre = () => {
         <h2 className="text-3xl">Sobre mim</h2>
         <div className="h-4/5 w-full flex max-md:justify-center">
           <div className="w-1/2 flex flex-col items-center justify-center max-md:hidden">
-            <p className="">Developer</p>
-            <p className="">Web</p>
-            <p className="">Front-End</p>
+            <p className="text-6xl">Developer</p>
+            <p className="text-4xl">Web</p>
+            <p className="text-4xl">Front-End</p>
           </div>
           <div className="w-1/2 p-5 bg-gray-800 rounded-xl shadow-white shadow-sombraLadosIguais max-md:w-[90%]">
             <p className="text-center">Olá, me chamo João Vitor, sou um jovem entusiasta da tecnologia com uma paixão por aprender e crescer na área, atualmente sou aluno na ADA e estou estudando desenvolvimento Front-End com ReactJS.</p>
